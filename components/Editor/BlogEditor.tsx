465f4ccb54fdc586641f58a48830ca046afa1734
satyam <satyam20_ug@ee.nits.ac.in>
2026-02-14 09:50:09 +0530

    added basic editor

--- components/Editor/BlogEditor.tsx
+++ components/Editor/BlogEditor.tsx
@@ -1,10 +1,30 @@
+"use client"
 
+// import { TextStyleKit } from '@tiptap/extension-text-style'
+import { useEditor, EditorContent } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+//starterkit gives us collection of all the popular extensions 
 
 export default function BlogEditor(){
+    const editor = useEditor({
+        extensions: [StarterKit],
+        content: `
+            <p>This isn’t bold.</p>
+            <p><strong>This is bold.</strong></p>
+            <p><b>And this.</b></p>
+            <p style="font-weight: bold">This as well.</p>
+            <p style="font-weight: bolder">Oh, and this!</p>
+            <p style="font-weight: 500">Cool, isn’t it!?</p>
+            <p style="font-weight: 999">Up to font weight 999!!!</p>
+          `,
+        shouldRerenderOnTransaction: true,
+        immediatelyRender: true,
+      })
+
     return (
         <div>
             <h1>Blog Editor</h1>
+            <EditorContent editor={editor} />
         </div>
     )
-
 }
