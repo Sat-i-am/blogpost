404658970f2b859c37de3445afbd6e3e20282a18
satyam <satyam20_ug@ee.nits.ac.in>
2026-02-14 10:07:25 +0530

    basic menubar added.

--- components/Editor/BlogEditor.tsx
+++ components/Editor/BlogEditor.tsx
@@ -1,21 +1,47 @@
 "use client"
 
 // import { TextStyleKit } from '@tiptap/extension-text-style'
+import Highlight from '@tiptap/extension-highlight'
+import TextAlign from '@tiptap/extension-text-align'
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import MenuBar from './Menubar'
 //starterkit gives us collection of all the popular extensions 
 
 export default function BlogEditor(){
     const editor = useEditor({
-        extensions: [StarterKit],
+        extensions: [
+            StarterKit,
+            TextAlign.configure({
+              types: ['heading', 'paragraph'],
+            }),
+            Highlight,
+          ],
         content: `
-            <p>This isn’t bold.</p>
-            <p><strong>This is bold.</strong></p>
-            <p><b>And this.</b></p>
-            <p style="font-weight: bold">This as well.</p>
-            <p style="font-weight: bolder">Oh, and this!</p>
-            <p style="font-weight: 500">Cool, isn’t it!?</p>
-            <p style="font-weight: 999">Up to font weight 999!!!</p>
+            <h3 style="text-align:center">
+        Devs Just Want to Have Fun by Cyndi Lauper
+      </h3>
+      <p style="text-align:center">
+        I come home in the morning light<br>
+        My mother says, <mark>“When you gonna live your life right?”</mark><br>
+        Oh mother dear we’re not the fortunate ones<br>
+        And devs, they wanna have fun<br>
+        Oh devs just want to have fun</p>
+      <p style="text-align:center">
+        The phone rings in the middle of the night<br>
+        My father yells, "What you gonna do with your life?"<br>
+        Oh daddy dear, you know you’re still number one<br>
+        But <s>girls</s>devs, they wanna have fun<br>
+        Oh devs just want to have
+      </p>
+      <p style="text-align:center">
+        That’s all they really want<br>
+        Some fun<br>
+        When the working day is done<br>
+        Oh devs, they wanna have fun<br>
+        Oh devs just wanna have fun<br>
+        (devs, they wanna, wanna have fun, devs wanna have)
+      </p>
           `,
         shouldRerenderOnTransaction: true,
         immediatelyRender: true,
@@ -23,7 +49,7 @@ export default function BlogEditor(){
 
     return (
         <div>
-            <h1>Blog Editor</h1>
+            <MenuBar editor={editor}/>
             <EditorContent editor={editor} />
         </div>
     )
